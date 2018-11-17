b5667bcff2639fb353fd76d672b24ed691d88768
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2018-11-16 20:00:13 -0800

    feat(homepage): add privary page

--- public/homepage/src/web/index.ts
+++ public/homepage/src/web/index.ts
@@ -10,4 +10,11 @@ export async function SetWebRoutes(server: Server) {
             return h.view('index');
         },
     });
+    server.route({
+        path: '/privacy',
+        method: 'GET',
+        handler(request, h) {
+            return h.view('pages/privacy', null, { layout: 'no_footer' });
+        },
+    });
 }
