4833f93137870b91a002da3ae72bf606bcc45f75
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2019-01-06 07:32:52 -0800

    add edit resolve

--- private/client/src/app/app-routing.module.ts
+++ private/client/src/app/app-routing.module.ts
@@ -1,12 +1,33 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule, Injectable } from '@angular/core';
+import { Routes, RouterModule, Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { ListComponent } from './pages/list/list.component';
 import { EditComponent } from './pages/edit/edit.component';
 import { PagesModule } from './pages/pages.module';
+import { ContentNS } from './domain/content';
+
+@Injectable({
+  providedIn: 'root'
+})
+class ContentResolver implements Resolve<ContentNS.Content> {
+  constructor(private repo: ContentNS.Repo) { }
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): |Promise<any> | any {
+    return this.repo.Get(route.params.content_id);
+  }
+}
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: ListComponent },
-  { path: ':content_id/:page_id', component: EditComponent }
+  {
+    path: ':content_id/:page_id',
+    component: EditComponent,
+    resolve: {
+      content: ContentResolver
+    }
+  }
 ];
 
 @NgModule({
--- private/client/src/app/pages/edit/edit.component.ts
+++ private/client/src/app/pages/edit/edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ContentNS } from 'src/app/domain/content';
 
 @Component({
   selector: 'app-edit',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.scss']
 })
 export class EditComponent implements OnInit {
+  content: ContentNS.Content;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.content = this.route.snapshot.data.content;
   }
 
 }
