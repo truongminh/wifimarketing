2c1ac9c6f6edcf905b533f3fad83f8360c411795
Tan Vuong Duc <tanvd54cntt@gmail.com>
2019-01-08 23:28:03 +0700

    refactor code

--- private/client/src/app/objs/objs-host.directive.ts
+++ private/client/src/app/objs/objs-host.directive.ts
@@ -3,6 +3,12 @@ import { SimpleTextComponent } from './simple-text/simple-text.component';
 import { SimpleImgComponent } from './simple-img/simple-img.component';
 import { ObjNS } from '../domain/obj';
 
+const mapComp = {
+  text: SimpleTextComponent,
+  image: SimpleImgComponent,
+  // input: SimpleTextComponent,
+}
+
 @Directive({
   selector: '[objsHost]'
 })
@@ -11,34 +17,13 @@ export class ObjsHostDirective {
   constructor(
     private componentFactoryResolver: ComponentFactoryResolver,
     private ref: ViewContainerRef,
-    public viewContainerRef: ViewContainerRef,
   ) { }
 
-  @Input() set data(data: ObjNS.Text | ObjNS.Image | ObjNS.Input) {
+  @Input() set data(data: ObjNS.Obj) {
     if (data) {
-      switch (data.type) {
-        case 'text': {
-          let componentFactory = this.componentFactoryResolver.resolveComponentFactory(SimpleTextComponent);
-          let componentRef = this.ref.createComponent(componentFactory);
-          componentRef.instance.data = data as ObjNS.Text;
-          break;
-        }
-        case 'image': {
-          let componentFactory = this.componentFactoryResolver.resolveComponentFactory(SimpleImgComponent);
-          let componentRef = this.ref.createComponent(componentFactory);
-          componentRef.instance.data = data as ObjNS.Image;
-          break;
-        }
-        case 'input': {
-          console.log('aaaaaaaaaaaa');
-          // let componentFactory = this.componentFactoryResolver.resolveComponentFactory(SimpleTextComponent);
-          // let componentRef = this.ref.createComponent(componentFactory);
-          // componentRef.instance.data = data as ObjNS.Input;
-          break;
-        }
-        default:
-          break;
-      }
+      let componentFactory = this.componentFactoryResolver.resolveComponentFactory(mapComp[data.type]);
+      let componentRef = this.ref.createComponent(componentFactory);
+      componentRef.instance['data'] = data;
     }
   }
 
