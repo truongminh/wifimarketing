1efbf1ed84d491c0c392abf3662b9c83b4e1727e
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2019-02-21 06:40:28 -0800

    add docker

--- internal/platform/setup-docker.sh
+++ internal/platform/setup-docker.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
+# add user wifi 
 useradd wifi
-curl https://get.docker.com | sh
+usermod -aG wheel wifi
+
+# install docker
+#curl https://get.docker.com | sh
+curl https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-18.06.2.ce-3.el7.x86_64.rpm -o docker-ce-18.06.2.ce-3.el7.x86_64.rpm
+yum install docker-ce-18.06.2.ce-3.el7.x86_64.rpm
 usermod -aG docker wifi
 systemctl enable docker
 systemctl start docker
 
+# install docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
 chmod +x /usr/bin/docker-compose
